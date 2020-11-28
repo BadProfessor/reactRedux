f5967c477015814941f5840f2b6d3e85fbca6ab8
Juraj Jarmek <badjjj@live.com>
2020-11-28 18:22:35 +0100

    jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,23 +1,54 @@
 // Import the React and ReactDOM libraries
-// React
+// import React
+// we are using webpack which makes every file existing for itself
+// every file is not automatically shared with other
+// if we want to use components from react we have to import the functionality
+// import: we want to reach a dependency
+// React: variable we want to assign to the import, we can name it differently
+// from: keyword that specifies that we need to pull code from somewhere
+// 'react' from which file or folder we are pulling from
+// require are CommonJS modules system import statements
+// import is a ES2015 Module systems
 import React from 'react';
-// ReactDOM
+// import ReactDOM, capital DOM
 import ReactDOM from 'react-dom';
 
 // Create a react component
 // name it App
+// arrow function
 const App = () => {
-  // Button text const
-  const buttonText = { text: 'Click me' };
-  const labelText = 'Enter name:';
+  // Button string const
+  const buttonText = { text: `Don't click` };
+  // const string for the label
+  const labelText = `Write something: `;
 
   return (
+    // reutrning a DIV
+    // this is not HTML
+    // we are using Babel to convert to ES5
+    // the browser doesn't understand JSX/React
+    // babeljs.io explains what is exactly happening when we compile from ES6+ to es5
+    // in ES5 this returns a JS function call
+    // we are not writing direct function calls because it can get way more complicated, and we go around that with the JSX
+    // special dialect of JS/Not HTML
+    // browsers don't understand JSX
+    // JSX very similar to HTML, but it has differences
+    // we are entering the text with braces and variables
+    // we don't need the actual .jsx filename
+    // convention: double quotes for jsx properties, single quotes for everything else
     <div>
       <label className="label" htmlFor="name">
         {labelText}
       </label>
+      <br />
       <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+      <br />
+      {
+        // we need to use custom inline styling for React
+        // whenever we have any dashes in our styling we need to use snakeCase
+        // we wrap the values in strings
+      }
+      <button style={{ backgroundColor: 'pink', color: 'black' }}>
         {buttonText.text}
       </button>
     </div>
@@ -26,4 +57,5 @@ const App = () => {
 
 // Take the react component and show it on the screen
 // root id
+// query selector for the root ID
 ReactDOM.render(<App />, document.querySelector('#root'));
