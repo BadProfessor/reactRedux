bc07d105a7e6f1999d7ec18bb8d58073e48bf6f0
Juraj Jarmek <badjjj@live.com>
2021-01-27 22:57:10 +0000

    widgets

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
+// making a link component to be put in the header
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    // fixing control keys functions
     if (event.metaKey || event.ctrlKey) {
       return;
     }
 
+    // preventing the default refresh
     event.preventDefault();
     window.history.pushState({}, '', href);
 
+    // communicating to the route components that a route has changed
     const navEvent = new PopStateEvent('popstate');
     window.dispatchEvent(navEvent);
   };
