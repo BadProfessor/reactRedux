437cd26ad764a57c843d5be15255b6b087ec693d
Juraj Jarmek <badjjj@live.com>
2021-02-22 22:53:26 +0000

    streams

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -6,8 +6,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-const compseEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
-const store = createStore(reducers, compseEnhancers(applyMiddleware()));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDom.render(
   <Provider store={store}>
